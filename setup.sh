78020d6b0f26cae673082877741e0843bcff7f76
SickoOrange <181536584@qq.com>
2017-10-25 22:38:28 +0200

    add auto generated github ssh key and save the key in the Desktop github_ssh_key.txt

--- setup.sh
+++ setup.sh
@@ -187,18 +187,35 @@ echo "#######################################################################"
 echo ""
 
 
+while getopts "g: b: c:" arg #选项后面的冒号表示该选项需要参数
+do
+        case $arg in
+             g)
+                echo "a's arg:$OPTARG" #参数存在$OPTARG中
+                # configure github ssh public key
+                ssh-keygen -t rsa -b 4096 -C "$OPTARG"
+                eval "$(ssh-agent -s)"
+                ssh-add ~/.ssh/id_rsa
+                sudo apt-get install xclip
+                xclip -sel clip < ~/.ssh/id_rsa.pub
+                cat ~/.ssh/id_rsa > ~/desktop/github_ssh_key.txt
+                eval "$(ssh-agent -s)"
+                ssh-add
+                ;;
+             b)
+                echo "b's arg:$OPTARG"
+                ;;
+             c)
+                echo "c"
+                ;;
+             ?) #当有不认识的选项的时候arg为?
+            echo "unkonw argument"
+        exit 1
+        ;;
+        esac
+done
 
 
-
-# configure github ssh public key
-#ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
-#eval "$(ssh-agent -s)"
-#ssh-add ~/.ssh/id_rsa
-#sudo apt-get install xclip
-#xclip -sel clip < ~/.ssh/id_rsa.pub
-
-#eval "$(ssh-agent -s)"
-#ssh-add
 
 
 
